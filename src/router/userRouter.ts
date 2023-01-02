3bbb141cac25692ecf262b0607d232c3ae1e9d48
ecstt <casottielia@gmail.com>
2023-01-02 03:43:05 +0100

    router fix 2

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,11 +1,6 @@
 import express from "express";
 import { authToken } from "../middleware/token";
-import {
-  getUser,
-  createUser,
-  loginUser,
-  deleteUser,
-} from "../handlers/userHandle";
+import { createUser, loginUser, deleteUser } from "../handlers/userHandle";
 
 const userRouter = express.Router();
 // NOT REQUIRE TOKEN
